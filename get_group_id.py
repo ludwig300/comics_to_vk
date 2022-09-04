108e5baac2c97c3f39c7018c7f66c3fd76145c3b
ludwig300 <105148929+ludwig300@users.noreply.github.com>
2022-09-04 23:30:10 +0700

    Renamed the variable

--- get_group_id.py
+++ get_group_id.py
@@ -3,11 +3,11 @@ import argparse
 import requests
 
 
-def get_group_id(vk_token):
+def get_group_id(access_token):
     url = 'https://api.vk.com/method/groups.get'
     payload = {
         'extended': 0,
-        'access_token': vk_token,
+        'access_token': access_token,
         'v': '5.131'
     }
     response = requests.get(url, params=payload)
@@ -17,14 +17,14 @@ def get_group_id(vk_token):
 
 def createParser():
     parser = argparse.ArgumentParser(description='Get "group_id"')
-    parser.add_argument('vk_token', help='`vk_token` can be obtained using `get_token_vk.py`')
+    parser.add_argument('access_token', help='`access_token` can be obtained using `get_token_vk.py`')
     return parser
 
 
 def main():
     parser = createParser()
     namespace = parser.parse_args()
-    print('group_id:', get_group_id(namespace.vk_token))
+    print('group_id:', get_group_id(namespace.access_token))
 
 
 if __name__ == '__main__':
